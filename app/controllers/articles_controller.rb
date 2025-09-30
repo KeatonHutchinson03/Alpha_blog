be6730aaa1f6953cdc95914db199da205a88f059
Keaton Hutchinson <keatonhutchinson03@gmail.com>
2025-09-30 09:17:04 -0400

    Add Show feature of articles

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,9 @@
+class ArticlesController < ApplicationController
+    
+    def show
+        @articles = Article.find(params[:id])
+    end
+    def index
+        @articles = Article.all
+    end
+end
