a91c9a8eb8c3f368659e49dd98e5d38133672645
Keaton Hutchinson <keatonhutchinson03@gmail.com>
2025-10-03 14:14:36 -0400

    Add users table and feature branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_09_29_191318) do
+ActiveRecord::Schema.define(version: 2025_10_03_175833) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title", null: false
     t.text "description", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
