f2a7903c4e13e0ca3207b38bb9916423d0ae23eb
Keaton Hutchinson <keatonhutchinson03@gmail.com>
2025-10-06 15:34:40 -0400

    Added show page for users and cleaned up links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         @articles = @user.articles
     end
+
+    def index
+        @users = User.all
+    end
     
     def new
         @user = User.new
@@ -16,7 +20,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:success] = "Your account was updated successfully"
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'edit'
         end
@@ -26,7 +30,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             flash[:success] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'new'
         end
